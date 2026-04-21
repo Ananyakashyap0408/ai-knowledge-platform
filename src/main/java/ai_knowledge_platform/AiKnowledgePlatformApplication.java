62150186fe585226d400e9d246c983c91434167c
Ananya Kashyap <ananyakashyap@Ananyas-MacBook-Air.local>
2026-04-22 02:36:27 +0530

    Day 1: Spring Boot setup + first API

---
+++ src/main/java/ai_knowledge_platform/AiKnowledgePlatformApplication.java
@@ -0,0 +1,13 @@
+package ai_knowledge_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiKnowledgePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiKnowledgePlatformApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ai_knowledge_platform/HelloController.java
@@ -0,0 +1,13 @@
+package ai_knowledge_platform;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Project is working 🚀";
+    }
+}
